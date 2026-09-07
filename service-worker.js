eb3a1ff53396fad38f307ea5a47e958dff63b511
PlanetWindows <planetwindowsmirandola@gmail.com>
2026-09-07 14:19:44 +0200

    Bump PWA cache for updated menu labels

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pw-posa-shell-v16';
+const CACHE_NAME = 'pw-posa-shell-v17';
 const BADGE_STATE_CACHE = 'pw-posa-badge-state-v1';
 const BADGE_STATE_URL = new URL('./__pw_posa_badge_count__', self.location.href).href;
 const APP_SHELL = [
